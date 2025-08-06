22cf43a16a44061f7740715d059a10d3c8fbc915
andre <andre@ANDREJ>
2025-08-06 21:54:48 +0200

    Add project files.

---
+++ Coding.Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿//Sales Analysis instructions
+//You are given a list of items sold in a store.
+
+//Each item is represented as a string with the item name and its price separated by a colon, like Apple:2.5.
+
+//Write a program that parses these strings and calculates the total income from all the sales.
+
+//Make sure to use const to represent the separator character (colon), and readonly to store the list of items sold.
+
+//The program should also handle edge cases such as incorrect formats, negative prices, and missing prices.
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding.Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Coding.Exercise/SalesAnalysis.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coding.Exercise
+{
+    internal class SalesAnalysis
+    {
+        public readonly List<string> ItemsSold;
+
+        public const char Separator = ':';
+
+        public SalesAnalysis(List<string> itemsSold)
+        {
+            ItemsSold = itemsSold;
+        }
+        public double ComputeAllIncome()
+        {
+            if (ItemsSold == null || !ItemsSold.Any()) return 0;
+
+            double total = 0;
+
+            foreach (var item in ItemsSold) {
+                var Item = item.Split(Separator);
+                if (Item.Length < 2 || string.IsNullOrWhiteSpace(Item[1])) continue;
+
+                if (double.TryParse(Item[1], out double price){
+                    if (price > 0)
+                    {
+                        total += price;
+                    }
+                }
+
+            }
+            return total;
+        }
+        
+    }
+}
