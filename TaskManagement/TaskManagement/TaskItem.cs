2e9013dde974950bc915feb2ea6e6f06259e56e9
andre <andrej2005milosavljevic@gmail.com>
2025-08-11 16:30:01 +0200

    TaskManagement

---
+++ TaskManagement/TaskManagement/TaskItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement
+{
+    public enum TaskStatus
+    {
+        NotStarted,
+        InProgress,
+        Completed,
+        OnHold
+    }
+    internal class TaskItem
+    {
+        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public TaskStatus Status { get; set; }
+
+
+    }
+}
---
+++ TaskManagement/TaskManagement/TaskManagement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement
+{
+    internal class TaskManagement
+    {
+        public List<TaskItem> tasks = new List<TaskItem>();
+
+        public void AddTask(int id, string title, DateTime dueDate,TaskStatus status)
+        {
+            tasks.Add(new TaskItem
+            {
+                Id = id,
+                Title = title,
+                DueDate = dueDate,
+                Status = status
+            });
+        }
+        public int TaskCount()
+        {
+            return tasks.Count;
+        }
+
+        public bool ChangeTaskStatus(int id, TaskStatus status)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+            if (task != null)
+            {
+                task.Status = status;
+                return true;
+            }
+            return false;
+        }
+
+        public TaskItem FindTaskById(int id)
+        {
+            return tasks.FirstOrDefault(t => t.Id == id);
+        }
+        public List<TaskItem> GetTasksDueToday()
+        {
+            return tasks
+                .Where(t => t.DueDate.Date == DateTime.Today)
+                .OrderBy(t => t.DueDate)
+                .ToList();
+        }
+
+
+
+
+    }
