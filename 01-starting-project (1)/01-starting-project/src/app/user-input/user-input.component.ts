865f775903d1dfa101ab0b26c284b7f25fc8c678
andre <andrej2005milosavljevic@gmail.com>
2025-09-14 21:24:48 +0200

    investment calculator

---
+++ 01-starting-project (1)/01-starting-project/src/app/user-input/user-input.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrls: ['./user-input.component.css'],
+})
+export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  constructor(private investmentService: InvestmentService) {}
+
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration,
+    });
+    this.enteredInitialInvestment = '0';
+    this.enteredAnnualInvestment = '0';
+    this.enteredExpectedReturn = '5';
+    this.enteredDuration = '10';
+  }
+}
